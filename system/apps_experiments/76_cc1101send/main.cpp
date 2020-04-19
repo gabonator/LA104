18ef6836bac1785bb81df96ea444ddbddbe7ef12
Gabriel <gvalky@sygic.com>
2020-04-19 15:26:09 +0200

    code cleanup

--- system/apps_experiments/76_cc1101send/main.cpp
+++ system/apps_experiments/76_cc1101send/main.cpp
@@ -27,69 +27,39 @@ bool send()
 {
   struct packet_t
   {
-    int length{2};
+    int length{12};
     uint8_t data[32];
   } packet;
 
   packet.data[0] = 0xaa;
-  packet.data[1] = 0xaa;
+  packet.data[1] = 0x00;
+  packet.data[2] = 0xaa;
+  packet.data[3] = 0xff;
+  packet.data[4] = 0xaa;
+  packet.data[5] = 0x00;
+  packet.data[6] = 0xaa;
+  packet.data[7] = 0xff;
 
   uint8_t marcState;
   int res = 0;
 
-//    gModem.SetIdleState();
-//    gModem.Calibrate();
-//BIOS::SYS::DelayMs(5);
-//  gModem.delayMicroseconds(500);
-
-/*
-  gModem.SetRxState();
-
-  while (((marcState = gModem.Read(CC1101_MARCSTATE | CC1101_STATUS_REGISTER)) & 0x1F) != 0x0D)
-  {
-    CONSOLE::Color(RGB565(808080));
-    CONSOLE::Print("(marc1=0x%02x)", marcState);
-    CONSOLE::Color(RGB565(ffffff));
-
-    if (marcState == 0x11)        // RX_OVERFLOW
-      gModem.FlushRxFifo();
-  }
-
-  gModem.delayMicroseconds(500);
-*/
-/*
-  marcState = gModem.Read(CC1101_MARCSTATE | CC1101_STATUS_REGISTER) & 0x1F;
-    CONSOLE::Print("(marc:%x) ", marcState); BIOS::SYS::DelayMs(5);
-  marcState = gModem.Read(CC1101_MARCSTATE | CC1101_STATUS_REGISTER) & 0x1F;
-    CONSOLE::Print("(marc:%x) ", marcState); BIOS::SYS::DelayMs(5);
-  marcState = gModem.Read(CC1101_MARCSTATE | CC1101_STATUS_REGISTER) & 0x1F;
-    CONSOLE::Print("(marc:%x) ", marcState); BIOS::SYS::DelayMs(5);
-  marcState = gModem.Read(CC1101_MARCSTATE | CC1101_STATUS_REGISTER) & 0x1F;
-    CONSOLE::Print("(marc:%x) ", marcState); BIOS::SYS::DelayMs(5);
-  */
   if (packet.length > 0)
   {
-    CONSOLE::Print("sending %d bytes ", packet.length);
+    CONSOLE::Print("%d bytes ", packet.length);
     gModem.SetDataRate(1000);
     gModem.SetOutputPower(0x50);
     gModem.SetPacketLength(packet.length);
-    gModem.Write(CC1101_TXFIFO, packet.data, packet.length);
+    gModem.Write(CC1101_TXFIFO, packet.data, packet.length); // max 64 bytes
 
     // CCA enabled: will enter TX state only if the channel is clear
     gModem.SetTxState();
   }
 
-//BIOS::SYS::DelayMs(5);
-//  gModem.delayMicroseconds(8500);
-
   // Check that TX state is being entered (state = RXTX_SETTLING)
   do {
+    // add delay, enabling TX goes through calibration, or disable automatic calibration
     marcState = gModem.Read(CC1101_MARCSTATE | CC1101_STATUS_REGISTER) & 0x1F;
   } while ((marcState != 0x13) && (marcState != 0x14) && (marcState != 0x15));
-
-//    CONSOLE::Color(RGB565(808080));
-//    CONSOLE::Print("(marc2=0x%02x)", marcState);
-//    CONSOLE::Color(RGB565(ffffff));
 
   if((marcState != 0x13) && (marcState != 0x14) && (marcState != 0x15))
   {
@@ -103,21 +73,15 @@ bool send()
     return false;
   }
 
-  // Wait for the sync word to be transmitted
-//  CONSOLE::Print(".");
-
-//  while (!gModem.Read());
-//  CONSOLE::Print(".");
-
-  // Wait until the end of the packet transmission
- // while (gModem.Read());
-//  CONSOLE::Print(".");
-
-  // Check that the TX FIFO is empty
+  CONSOLE::Color(RGB565(b0b0b0));
   int txbytes = 0;
   do {
+    // add delay, or sync with GDO0
     txbytes = gModem.Read(CC1101_TXBYTES | CC1101_STATUS_REGISTER) & 0x7F;
+    CONSOLE::Print("%d ", txbytes);
+    BIOS::SYS::DelayMs(8);
   } while (txbytes > 0);
+  CONSOLE::Color(RGB565(ffffff));
 
   if(txbytes == 0)
     res = true;
@@ -126,9 +90,6 @@ bool send()
     CONSOLE::Print("(not sent, tx bytes=%d)", txbytes);
     CONSOLE::Color(RGB565(ffffff));
   }
-
-//    gModem.SetIdleState();
-//    gModem.FlushTxFifo();
 
   return res;
 }
