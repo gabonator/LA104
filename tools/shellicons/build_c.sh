240752d6975d3a50ebf8926af431b772eb110b9e
Gabriel <gvalky@sygic.com>
2020-11-02 20:30:59 +0100

    magick not found on docker image

--- tools/shellicons/build_c.sh
+++ tools/shellicons/build_c.sh
@@ -1,39 +1,35 @@
-#!/bin/bash
-set -x
-magick=""
+#convert folder.png -resize 64x64 -alpha off -background "#ff00ff" -flatten -colors 256 -compress none folder_min.bmp
+convert folder.png -resize 64x64 -background transparent -gravity Center -extent 64x64 compo1.png
+convert games.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png fgames.bmp
 
-#magick convert folder.png -resize 64x64 -alpha off -background "#ff00ff" -flatten -colors 256 -compress none folder_min.bmp
-magick convert folder.png -resize 64x64 -background transparent -gravity Center -extent 64x64 compo1.png
-magick convert games.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png fgames.bmp
+convert chip.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png fchip.bmp
 
-magick convert chip.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png fchip.bmp
+convert settings.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png fsett.bmp
 
-magick convert settings.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png fsett.bmp
+convert fractal.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png ffractal.bmp
 
-magick convert fractal.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png ffractal.bmp
+convert oscillo.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png foscillo.bmp
 
-magick convert oscillo.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png foscillo.bmp
+convert atom.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png fatom.bmp
 
-magick convert atom.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png fatom.bmp
+convert usb.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png fusb.bmp
 
-magick convert usb.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png fusb.bmp
+convert binary.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png fbinary.bmp
 
-magick convert binary.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png fbinary.bmp
+convert measure.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
+convert -composite compo1.png compo2.png fmeasure.bmp
 
-magick convert measure.png -resize 32x32 -background transparent -gravity SouthEast -extent 64x64 compo2.png
-magick convert -composite compo1.png compo2.png fmeasure.bmp
+convert manager.png -resize 60x60 -background transparent -gravity Center -extent 64x64 manager.bmp
 
-magick convert manager.png -resize 60x60 -background transparent -gravity Center -extent 64x64 manager.bmp
-
-magick convert chip.png -resize 60x60 -background transparent -gravity Center -extent 64x64 chip.bmp
+convert chip.png -resize 60x60 -background transparent -gravity Center -extent 64x64 chip.bmp
 
 #https://www.clipartmax.com/download/m2i8H7K9N4i8N4m2_free-vector-or-logic-functions-digital-electronics-digital-electronics/
 
--- tools/shellicons/build_d.sh
+++ tools/shellicons/build_d.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-magick=""
-
-magick convert fractal.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on fractal.bmp
-magick convert mouse.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on mouse.bmp
-magick convert midi.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on midi.bmp
-magick convert charmap.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on charmap.bmp
-magick convert serial.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on serial.bmp
-magick convert imgAnalyser.png -crop 128x128+0+0 -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on official.bmp
-magick convert imgSeq.png -crop 128x128+0+0 -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on sequence.bmp
-magick convert application.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on app.bmp
-magick convert pulseview.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on sump.bmp
-magick convert genie.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on genie.bmp
-magick convert measure.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on measure.bmp
-magick convert magnifier.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on magnifier.bmp
+convert fractal.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on fractal.bmp
+convert mouse.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on mouse.bmp
+convert midi.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on midi.bmp
+convert charmap.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on charmap.bmp
+convert serial.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on serial.bmp
+convert imgAnalyser.png -crop 128x128+0+0 -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on official.bmp
+convert imgSeq.png -crop 128x128+0+0 -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on sequence.bmp
+convert application.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on app.bmp
+convert pulseview.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on sump.bmp
+convert genie.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on genie.bmp
+convert measure.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on measure.bmp
+convert magnifier.png -resize 64x64 -background transparent -gravity Center -extent 64x64 -alpha on magnifier.bmp
