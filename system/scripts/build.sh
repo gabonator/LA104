0cc1c75c973a126618339eeda64422dc214f4937
Gabriel <gvalky@sygic.com>
2020-12-11 01:02:30 +0100

    temporarily disabled applist generator

--- system/scripts/build.sh
+++ system/scripts/build.sh
@@ -2,4 +2,7 @@
 ./release_image_ds203.sh $1
 ./release_image_ds213.sh $1
 ./release_image_la104.sh $1
-./build_applist.sh $1
+
+node -v
+# applist disabled because of old nodejs version in docker builds
+# ./build_applist.sh $1
