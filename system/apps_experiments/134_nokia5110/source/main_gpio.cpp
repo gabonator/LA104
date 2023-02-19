eaa4964a6af1683d501ae634904ea07ea8a802bb
Gabriel Valky <gabriel@valky.eu>
2023-02-19 10:22:45 +0100

    nokia display showing text and inversion

--- system/apps_experiments/134_nokia5110/source/main_gpio.cpp
+++ system/apps_experiments/134_nokia5110/source/main_gpio.cpp
@@ -119,6 +119,7 @@ void logoDisp(unsigned char logo[])
 
 CNokia5110 display;
 uint8_t buf[504];
+uint16_t temp[84];
 
 __attribute__((__section__(".entry")))
 int main(void)
@@ -129,20 +130,69 @@ int main(void)
     CONSOLE::Print("P3 - DIN\n");
     CONSOLE::Print("P4 - CLK\n");
     CONSOLE::Print("GND - CE\n");
+    CRect rcWindow(140, 40, 140+84, 40+48);
+    rcWindow.Inflate(1, 1, 1, 1);
+    BIOS::LCD::Rectangle(rcWindow, RGB565(b0b0b0));
+    rcWindow.Deflate(1, 1, 1, 1);
+    BIOS::LCD::Bar(rcWindow, RGB565(404040));
+
+    BIOS::LCD::Print(rcWindow.left, rcWindow.top-2  , RGB565(ffffff), RGBTRANS, "Hello 1234");
+    BIOS::LCD::Print(rcWindow.left, rcWindow.top+10, RGB565(ffffff), RGBTRANS, "ABCDEFGHIJ");
+    BIOS::LCD::Print(rcWindow.left, rcWindow.top+22, RGB565(ffffff), RGBTRANS, "abcdefghij");
+    BIOS::LCD::Print(rcWindow.left, rcWindow.top+34, RGB565(ffffff), RGBTRANS, "!@#$%^&*()");
+
+    int animt = 0;
+    int animi = 0;
 //enum {DC = BIOS::GPIO::P2, CE = -1, CLK = BIOS::GPIO::P4, DIN = BIOS::GPIO::P3, RST = BIOS::GPIO::P1, LOW=0, HIGH=1, OUTPUT=2};
 
     display.Init();
-    int grow = 0;
     while (!BIOS::KEY::GetKey())
     {
-      for (int i=0; i<504; i++)
-        buf[i] = rand()&255;
-      for (int i=0; i<grow; i++)
-        buf[i] = 255;
-      if (grow<504)
-        grow++;
-      else
-        grow = 0;
+      CRect invert;
+      if (animt == 0)
+      {
+        invert = CRect(rcWindow.left+animi, rcWindow.top, rcWindow.left+animi+1, rcWindow.bottom);
+        if (++animi == 84)
+        {
+          display.Init();
+          animi = 0;
+          animt = 1;
+        }
+      } else { 
+        invert = CRect(rcWindow.left, rcWindow.top + animi, rcWindow.right, rcWindow.top + animi+1);
+        if (++animi == 48)
+        {
+          display.Init();
+          animi = 0;
+          animt = 0;
+        }
+      }
+      int l = invert.Width() + invert.Height()-1;
+      _ASSERT(l <= COUNT(temp));
+      BIOS::LCD::BufferBegin(invert);
+      BIOS::LCD::BufferRead(temp, l);
+      BIOS::LCD::BufferEnd();
+      for (int i=0; i<l; i++)
+        temp[i] = (temp[i] & 0x8000) ? RGB565(404040) : RGB565(ffffff);
+
+      BIOS::LCD::BufferBegin(invert);
+      BIOS::LCD::BufferWrite(temp, l);
+      BIOS::LCD::BufferEnd();
+      int i = 0;
+      for (int y=0; y<6; y++)
+        for (int x=0; x<84; x++, i++)
+        {
+          CRect pix(rcWindow.left+x, rcWindow.top+y*8, rcWindow.left+x+1, rcWindow.top+y*8+8);
+          BIOS::LCD::BufferBegin(pix);
+          BIOS::LCD::BufferRead(temp, l);
+          BIOS::LCD::BufferEnd();
+          uint8_t b = 0;
+          for (int j=0; j<8; j++)
+            if ((temp[7-j] & 0x8000))
+              b |= 1<<j;
+          buf[i] = b;
+        }
+
       display.logoDisp(buf);
 //      BIOS::SYS::DelayMs(20);
     }
